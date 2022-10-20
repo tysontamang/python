b324c54d632c8d885f62b711238202c859e76553
taisan tamang <54075970+tysontamang@users.noreply.github.com>
2022-10-20 13:17:45 +0545

    added follow

---
+++ alpha.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+echo ""
+
+echo -e "\e[1;91m╔══╗\e[1;92m┌──┐┬   ┬   ┌──┐┬ ┬ ┬"
+
+echo -e "\e[1;91m║   \e[1;92m│  ││   │   │  ││ │ │"
+
+echo -e "\e[1;91m╠═╣ \e[1;92m│  ││   │   │  ││ │ │"
+
+echo -e "\e[1;91m║   \e[1;92m└──┘┴──┘┴──┘└──┘└─┴─┘"
+
+echo -e "\e[1;91m╩\e[1;92m     \e[1;92mCOMPUTER ENGINEERING"
+python3 open.py
